0ef532abdf1f38c6849460f2245dc34bc39a4f0d
keithheiberg <46172311+keithheiberg@users.noreply.github.com>
2019-02-05 17:10:53 -0600

    Add files via upload

---
+++ C-sharp_p66/C-sharp_p66/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Please enter a number: ");
+        string firstStringNumber = Console.ReadLine();
+        double firstNumber = Convert.ToDouble(firstStringNumber);
+        double product = firstNumber * 50.0;
+        Console.WriteLine("Your number * 50 = " + product);
+
+        Console.WriteLine("Please enter another number: ");
+        string secondStringNumber = Console.ReadLine();
+        double secondNumber = Convert.ToDouble(secondStringNumber);
+        double sum = secondNumber + 25.0;
+        Console.WriteLine("Your number + 25 = " + sum);
+
+        Console.WriteLine("Please enter another number: ");
+        string thirdStringNumber = Console.ReadLine();
+        double thirdNumber = Convert.ToDouble(thirdStringNumber);
+        double quotient = thirdNumber / 12.5;
+        Console.WriteLine("Your number / 12.5 = " + quotient);
+
+        Console.WriteLine("Please enter another number: ");
+        string fourthStringNumber = Console.ReadLine();
+        double fourthNumber = Convert.ToDouble(fourthStringNumber);
+        bool overFifty = fourthNumber > 50.0;
+        Console.WriteLine("Is your number > 50? " + overFifty);
+
+        Console.WriteLine("Please enter another number: ");
+        string fifthStringNumber = Console.ReadLine();
+        double fifthNumber = Convert.ToDouble(fifthStringNumber);
+        double remainder = fifthNumber % 7.0;
+        Console.WriteLine("Your number / 7, remainder = " + remainder);
+
+        Console.ReadLine();
+    }
+}
